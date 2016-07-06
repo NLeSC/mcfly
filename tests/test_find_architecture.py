9ca84064933c21c2006e4f6a40cb62d9dad7cb4b
dafnevk <d.vankuppevelt@esciencecenter.nl>
2016-07-06 14:42:24 +0200

    tests for find_architecture #18 #31

---
+++ tests/test_find_architecture.py
@@ -0,0 +1,63 @@
+from mcfly import find_architecture
+import numpy as np
+from nose.tools import assert_equal, assert_equals, assert_almost_equal
+from keras.utils.np_utils import to_categorical
+
+import unittest
+
+
+class FindArchitectureSuite(unittest.TestCase):
+
+    """Basic test cases."""
+
+    def test_kNN_accuracy_1(self):
+        """
+        The accuracy for this single-point dataset should be 1.
+        """
+        X_train = np.array([[[1]], [[0]]])
+        y_train = np.array([[1, 0], [0, 1]])
+        X_val = np.array([[[0.9]]])
+        y_val = np.array([[1, 0]])
+
+        acc = find_architecture.kNN_accuracy(
+            X_train, y_train, X_val, y_val, k=1)
+        assert_almost_equal(acc, 1.0)
+
+    def test_kNN_accuracy_0(self):
+        """
+        The accuracy for this single-point dataset should be 0.
+        """
+        X_train = np.array([[[1]], [[0]]])
+        y_train = np.array([[1, 0], [0, 1]])
+        X_val = np.array([[[0.9]]])
+        y_val = np.array([[0, 1]])
+
+        acc = find_architecture.kNN_accuracy(
+            X_train, y_train, X_val, y_val, k=1)
+        assert_almost_equal(acc, 0)
+
+    def test_find_best_architecture(self):
+        num_timesteps = 100
+        num_channels = 2
+        num_samples_train = 5
+        num_samples_val = 3
+        X_train = np.random.rand(
+            num_samples_train,
+            num_timesteps,
+            num_channels)
+        y_train = to_categorical(np.array([0, 0, 1, 1, 1]))
+        X_val = np.random.rand(num_samples_val, num_timesteps, num_channels)
+        y_val = to_categorical(np.array([0, 1, 1]))
+        best_model, best_params, best_model_type, knn_acc = find_architecture.find_best_architecture(
+            X_train, y_train, X_val, y_val, verbose=True,
+            number_of_models=1, nr_epochs=1)
+        assert hasattr(best_model, 'fit')
+        self.assertIsNotNone(best_params)
+        self.assertIsNotNone(best_model_type)
+        assert knn_acc >= 0 and knn_acc <= 1
+
+    def setUp(self):
+        np.random.seed(1234)
+
+if __name__ == '__main__':
+    unittest.main()
