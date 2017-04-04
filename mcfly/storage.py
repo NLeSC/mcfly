a35b87ab0916652ccd9220674dd4641397677a75
dafnevk <d.vankuppevelt@esciencecenter.nl>
2017-04-04 13:51:26 +0200

    fixed path concatenation #122

--- mcfly/storage.py
+++ mcfly/storage.py
@@ -8,6 +8,7 @@
 from keras.models import model_from_json
 import json
 import numpy as np
+import os
 
 
 def savemodel(model, filepath, modelname):
@@ -30,12 +31,12 @@ def savemodel(model, filepath, modelname):
         Path to npy file with weights
     """
     json_string = model.to_json()  # save architecture to json string
-    json_path = filepath + modelname + '_architecture.json'
+    json_path = os.path.join(filepath, modelname + '_architecture.json')
     with open(json_path, 'w') as outfile:
         json.dump(json_string, outfile, sort_keys=True, indent=4,
                   ensure_ascii=False)
     wweights = model.get_weights()  # get weight from model
-    numpy_path = filepath + modelname + '_weights'
+    numpy_path = os.path.join(filepath, modelname + '_weights')
     np.save(numpy_path,
             wweights)  # save weights in npy file
     return json_path, numpy_path
@@ -56,13 +57,13 @@ def loadmodel(filepath, modelname):
     model_repro : Keras object
         reproduced model
     """
-    with open(filepath + modelname + '_architecture.json', 'r') as outfile:
+    with open(os.path.join(filepath, modelname + '_architecture.json'), 'r') as outfile:
         json_string_loaded = json.load(outfile)
     model_repro = model_from_json(json_string_loaded)
     # wweights2 = model_repro.get_weights()
     #  extracting the weights would give us the untrained/default weights
     wweights_recovered = np.load(
-        filepath + modelname + '_weights.npy')  # load the original weights
+        os.path.join(filepath, modelname + '_weights.npy'))  # load the original weights
     model_repro.set_weights(wweights_recovered)  # now set the weights
     return model_repro
 
