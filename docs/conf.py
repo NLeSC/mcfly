8e1abce3133dba2bc78790439ad48c79bd8dfee4
dafnevk <d.vankuppevelt@esciencecenter.nl>
2018-11-29 13:22:51 +0100

    added log and different theme to docs

--- docs/conf.py
+++ docs/conf.py
@@ -118,7 +118,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'haiku'
+html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -137,7 +137,7 @@ html_theme = 'haiku'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = 'mcflylogo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
