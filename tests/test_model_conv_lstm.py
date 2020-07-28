06cc08502e7e8be593b64961c7c2e3677be340cd
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-07-28 15:15:23 +0200

    fix tests

--- tests/test_model_conv_lstm.py
+++ tests/test_model_conv_lstm.py
@@ -2,7 +2,7 @@ import numpy as np
 from mcfly.models.model_conv_lstm import Model_ConvLSTM
 
 
-def test_deepconvlstm_batchnorm_dim(self):
+def test_deepconvlstm_batchnorm_dim():
     """The output shape of the batchnorm should be (None, nr_timesteps, nr_channels, nr_filters)"""
     model_type = Model_ConvLSTM((None, 20, 3), 2)
     model = model_type.create_model(**{"filters": [32, 32],
@@ -11,7 +11,7 @@ def test_deepconvlstm_batchnorm_dim(self):
     assert batchnormlay.output_shape == (None, 20, 3, 32)
 
 
-def test_deepconvlstm_enough_batchnorm(self):
+def test_deepconvlstm_enough_batchnorm():
     """LSTM model should contain as many batch norm layers as it has activations layers"""
     model_type = Model_ConvLSTM((None, 20, 3), 2)
     model = model_type.create_model(**{"filters": [32, 32, 32],
@@ -22,7 +22,7 @@ def test_deepconvlstm_enough_batchnorm(self):
     assert batch_norm_layers == activation_layers
 
 
-def test_DeepConvLSTM_hyperparameters_nrconvlayers(self):
+def test_DeepConvLSTM_hyperparameters_nrconvlayers():
     """ Number of Conv layers from range [4, 4] should be 4. """
     custom_settings = self.get_default()
     settings = {'deepconvlstm_min_conv_layers': 4,
@@ -36,7 +36,7 @@ def test_DeepConvLSTM_hyperparameters_nrconvlayers(self):
     assert len(hyperparams.get('filters')) == 4, "Expected different filter number"
 
 
-def test_deepconvlstm_starts_with_batchnorm(self):
+def test_deepconvlstm_starts_with_batchnorm():
     """ DeepConvLSTM models should always start with a batch normalization layer. """
     model_type = Model_ConvLSTM((None, 20, 3), 2)
     model = model_type.create_model(**{"filters": [32, 32],
