7144db1a4ec69153cab2f7560fbb3ba68c404be7
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-07-28 11:52:09 +0200

    fix and extend cnn tests

--- tests/test_model_cnn.py
+++ tests/test_model_cnn.py
@@ -3,19 +3,21 @@ from mcfly.models import model_cnn
 
 
 # Tests for CNN model:
-def test_cnn_starts_with_batchnorm(self):
+def test_cnn_starts_with_batchnorm():
     """ CNN models should always start with a batch normalization layer. """
     model_type = model_cnn((None, 20, 3), 2)
     model = model_type.create_model({"filters": [32, 32],
                                      "fc_hidden_nodes": 100})
     assert 'BatchNormalization' in str(type(model.layers[0])), 'Wrong layer type.'
 
-# def test_cnn_fc_nodes(self):
-#     """ CNN model should have number of dense nodes defined by user. """
-#     fc_hidden_nodes = 101
-#     model = modelgen.generate_CNN_model((None, 20, 3), 2, [32, 32], fc_hidden_nodes)
-#     dense_layer = [l for l in model.layers if 'Dense' in str(l)][0]
-#     assert dense_layer.output_shape[1] == fc_hidden_nodes, 'Wrong number of fc nodes.'
+def test_cnn_fc_nodes():
+    """ CNN model should have number of dense nodes defined by user. """
+    fc_hidden_nodes = 101
+    model_type = model_cnn((None, 20, 3), 2)
+    model = model_type.create_model({"filters": [32, 32],
+                                     "fc_hidden_nodes": fc_hidden_nodes})
+    dense_layer = [l for l in model.layers if 'Dense' in str(l)][0]
+    assert dense_layer.output_shape[1] == fc_hidden_nodes, 'Wrong number of fc nodes.'
 
 # def test_cnn_batchnorm_dim(self):
 #     """"The output shape of the batchnorm should be (None, nr_timesteps, nr_filters)"""
