d47476e7ec2971f630fe01552e172983a57d5c30
florian-huber <36473328+florian-huber@users.noreply.github.com>
2020-07-30 16:15:07 +0200

    change model class names (remove model_)

--- mcfly/models/__init__.py
+++ mcfly/models/__init__.py
@@ -1,12 +1,12 @@
-from .model_cnn import Model_CNN
-from .model_conv_lstm import Model_ConvLSTM
-from .model_inception_time import Model_InceptionTime
-from .model_resnet import Model_ResNet
+from .cnn import CNN
+from .conv_lstm import ConvLSTM
+from .inception_time import InceptionTime
+from .resnet import ResNet
 
 
 __all__ = [
-    "Model_CNN",
-    "Model_ConvLSTM",
-    "Model_InceptionTime",
-    "Model_ResNet"
+    "CNN",
+    "ConvLSTM",
+    "InceptionTime",
+    "ResNet"
 ]
